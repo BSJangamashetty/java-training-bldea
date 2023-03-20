600d4887f19e19cf0f6bf8d519c9136925d97fb5
bhagy <bhagy@HP>
2023-03-20 20:32:53 +0530

    Seventh Commit

--- src/training_codes/Test5.java
+++ src/training_codes/Test5.java
@@ -1,10 +1,12 @@
 package training_codes;
-
-public class Test5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Test5 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print("* ");
+		}
 	}
 
 }
--- src/training_codes/Test7.java
+++ src/training_codes/Test7.java
@@ -1,11 +1,12 @@
 package training_codes;
-public class Test6 
+public class Test7 
 {
 	public static void main(String[] args) 
 	{
 		for(int i=1;i<=5;i++)
 		{
-			System.out.println("@ ");
+
+			System.out.print(i+" ");
 		}
 	}
 
