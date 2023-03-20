4ac1d2ec43972d2afe18ce0f4913494d63b1f955
bhagy <bhagy@HP>
2023-03-20 20:36:07 +0530

    Eighth Commit

---
+++ src/training_codes/Test8.java
@@ -0,0 +1,15 @@
+package training_codes;
+public class Test8 
+{
+	public static void main(String[] args) 
+	{
+		int count=1;
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(count+ " ");
+			count=count+5;
+		}
+
+	}
+
+}
