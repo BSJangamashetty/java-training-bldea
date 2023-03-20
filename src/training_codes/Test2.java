464fde161538fbe3387bd188e42024855c0d1568
bhagy <bhagy@HP>
2023-03-20 20:17:49 +0530

    Second Commit

---
+++ src/training_codes/Test2.java
@@ -0,0 +1,15 @@
+package training_codes;
+
+public class Test2 
+{
+
+	public static void main(String[] args) 
+	{
+		{
+			for(int i=1;i<=5;i++)
+				System.out.println("Hello World!");
+
+		}
+	}
+
+}
