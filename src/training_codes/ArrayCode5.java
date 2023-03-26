214c8e266c95c1054bd63d277d7d223e7f3f9c66
bhagy <bhagy@HP>
2023-03-26 22:12:17 +0530

    Array

---
+++ src/training_codes/ArrayCode5.java
@@ -0,0 +1,15 @@
+package training_codes;
+import java.util.Arrays;
+public class ArrayCode5 
+{
+	public static void main(String[] args) 
+	{
+		int arr[]= {10,30,25,68,40,15,90,78,144,71,54,8};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+", ");
+		}
+	}
+
+}
