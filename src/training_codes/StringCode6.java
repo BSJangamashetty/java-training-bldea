98c954124bcb41a8352044e275d0c2e75d6c4c7a
bhagy <bhagy@HP>
2023-03-28 08:15:06 +0530

    StringCode6 Commit

---
+++ src/training_codes/StringCode6.java
@@ -0,0 +1,17 @@
+package training_codes;
+public class StringCode6 
+{
+	public static void main(String[] args) 
+	{
+		String s1="RAMA";
+		String s2="RAMA";
+		if(s1==s2)
+		{
+			System.out.println("Adresses are equal");
+		}
+		else
+		{
+			System.out.println("Adresses not equal");
+		}
+	}
+}
