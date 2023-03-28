d8dd3aa9edbca9208ba71dd3d7dc9e58f4c9579e
bhagy <bhagy@HP>
2023-03-28 08:48:58 +0530

    StringCode7 Commit

---
+++ src/training_codes/StringCode7.java
@@ -0,0 +1,18 @@
+package training_codes;
+public class StringCode7 
+{
+	public static void main(String[] args)
+	{
+		String s1= new String("RAMA");
+		String s2= new String("RAMA");
+		if(s1==s2)
+		{
+			System.out.println("Adresses are equal");
+		}
+		else
+		{
+			System.out.println("Adresses not equal");
+		}
+	}
+
+}
