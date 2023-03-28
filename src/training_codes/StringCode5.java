d23f6c58b3b70eb040f68e4171c9862b00ac6d07
bhagy <bhagy@HP>
2023-03-28 08:06:43 +0530

    StringCode5 Commit

---
+++ src/training_codes/StringCode5.java
@@ -0,0 +1,17 @@
+package training_codes;
+public class StringCode5 
+{
+	public static void main(String[] args) 
+	{
+		String s1="RAMA";
+		String s2="RAMA";
+		if(s1.equals(s2))
+		{
+			System.out.println("String are equal");
+		}
+		else
+		{
+			System.out.println("String not equal");
+		}
+	}
+}
