53cf619cab06d090b5865b5101ae80c5951f65ba
bhagy <bhagy@HP>
2023-03-28 07:34:50 +0530

    StringCode2 Commit

---
+++ src/training_codes/StringCode2.java
@@ -0,0 +1,22 @@
+package training_codes;
+public class StringCode2 
+{
+	public static void main(String[] args)
+	{
+		String str="RajaRamMohanRoy";
+		System.out.println(str.length());
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+		System.out.println(str.charAt(1));
+		int a=10+str.charAt(1);
+		System.out.println(a);
+		System.out.println(str.indexOf('R'));
+		System.out.println(str.indexOf('z'));
+		System.out.println(str.startsWith("Raja"));
+		System.out.println(str.startsWith("Roja"));
+		System.out.println(str.endsWith("Roy"));
+		System.out.println(str.endsWith("Boy"));
+		System.out.println(str.contains("Mohan"));
+		System.out.println(str.contains("Rohan"));
+	}
+}
